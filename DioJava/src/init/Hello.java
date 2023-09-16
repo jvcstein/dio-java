abf1c1c35c75f268220100012c9d0724de7337ed
jvcstein <jvcorreiastein@gmail.com>
2023-09-16 13:06:34 -0300

    first commit

--- DioJava/src/init/Hello.java
+++ DioJava/src/init/Hello.java
@@ -3,7 +3,7 @@ package init;
 public class Hello {
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, Java World!");
+		System.out.println("Hello, Java Worlda!");
 		
 	}
 
